1d3da93445b7b3529e4ccd337635cf087f76cabf
Joonas Pessi <joonas.pessi@kalmarglobal.com>
2024-01-23 07:10:57 +0200

    day21 part2

--- src/21.py
+++ src/21.py
@@ -1,3 +1,7 @@
+from numpy import rint
+from numpy.polynomial.polynomial import polyfit
+
+
 def read_input(name: str) -> str:
     with open(name) as f:
         data = f.read()
@@ -15,30 +19,52 @@ def parse(input_data):
     return grid, sr, sc
 
 
-def step(grid, positions):
+def step(grid, positions, part2):
     len_r = len(grid)
     len_c = len(grid[0])
     new_positions = set()
     for r, c in positions:
         for dr, dc in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
-            if 0 <= r + dr < len_r and 0 <= c + dc < len_c and grid[r + dr][c + dc] != "#":
+            new_r, new_c = wrap(grid, r + dr, c + dc) if part2 else (r + dr, c + dc)
+            if 0 <= new_r < len_r and 0 <= new_c < len_c and grid[new_r][new_c] != "#":
                 new_positions.add((r + dr, c + dc))
     return new_positions
 
 
+def wrap(grid, r, c):
+    len_r = len(grid)
+    len_c = len(grid[0])
+    return r % len_r, c % len_c
+
+
 def part_1(input_data: str) -> int:
-    grid, sr, rc = parse(input_data)  # noqa
+    grid, sr, rc = parse(input_data)
 
     pos = set([(sr, rc)])
     for _ in range(64):
-        pos = step(grid, pos)
+        pos = step(grid, pos, False)
 
     return len(pos)
 
 
 def part_2(input_data: str) -> int:
-    data = parse(input_data)  # noqa
-    return 0
+    grid, sr, rc = parse(input_data)
+
+    size = len(grid)
+    size_2 = size // 2
+    y = []
+    pos = set([(sr, rc)])
+    for s in range(size_2 + size * 2 + 1):
+        if s % size == size_2:
+            y.append(len(pos))
+        pos = step(grid, pos, True)
+
+    x = [0, 1, 2]
+    poly = rint(polyfit(x, y, 2)).astype(int).tolist()
+
+    target = (26501365 - size_2) // size
+
+    return sum([poly[i] * target**i for i in range(3)])
 
 
 if __name__ == "__main__":
@@ -71,11 +97,43 @@ def test__part1():
 
 def test__part2_sample():
     input_data = """
-    xxx
+    .................................
+    .....###.#......###.#......###.#.
+    .###.##..#..###.##..#..###.##..#.
+    ..#.#...#....#.#...#....#.#...#..
+    ....#.#........#.#........#.#....
+    .##...####..##...####..##...####.
+    .##..#...#..##..#...#..##..#...#.
+    .......##.........##.........##..
+    .##.#.####..##.#.####..##.#.####.
+    .##..##.##..##..##.##..##..##.##.
+    .................................
+    .................................
+    .....###.#......###.#......###.#.
+    .###.##..#..###.##..#..###.##..#.
+    ..#.#...#....#.#...#....#.#...#..
+    ....#.#........#.#........#.#....
+    .##...####..##..S####..##...####.
+    .##..#...#..##..#...#..##..#...#.
+    .......##.........##.........##..
+    .##.#.####..##.#.####..##.#.####.
+    .##..##.##..##..##.##..##..##.##.
+    .................................
+    .................................
+    .....###.#......###.#......###.#.
+    .###.##..#..###.##..#..###.##..#.
+    ..#.#...#....#.#...#....#.#...#..
+    ....#.#........#.#........#.#....
+    .##...####..##...####..##...####.
+    .##..#...#..##..#...#..##..#...#.
+    .......##.........##.........##..
+    .##.#.####..##.#.####..##.#.####.
+    .##..##.##..##..##.##..##..##.##.
+    .................................
     """
-    assert part_2(input_data) == 0
+    assert part_2(input_data) == 475308805510348
 
 
 def test__part2():
     input_data = read_input("inputs/day21.txt")
-    assert part_2(input_data) == 0
+    assert part_2(input_data) == 639051580070841
