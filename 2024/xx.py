fd08340edf7e971ba0911c4dba0bacd710b6c693
Joonas Pessi <joonas.pessi@mapbox.com>
2024-12-09 14:12:15 +0200

    day template

---
+++ 2024/xx.py
@@ -0,0 +1,51 @@
+INPUT_FILE = "inputs/2024/dayXX.txt"
+
+
+def read_input(name: str) -> str:
+    with open(name) as f:
+        data = f.read()
+    return data.strip()
+
+
+def parse(input_data):
+    return input_data
+
+
+def part_1(input_data: str) -> int:
+    data = parse(input_data)  # noqa
+    return 0
+
+
+def part_2(input_data: str) -> int:
+    data = parse(input_data)  # noqa
+    return 0
+
+
+if __name__ == "__main__":
+    input_data = read_input(INPUT_FILE)
+    print(part_1(input_data))
+    print(part_2(input_data))
+
+
+def test__part1_sample():
+    input_data = """
+    xxx
+    """
+    assert part_1(input_data) == 0
+
+
+def test__part1():
+    input_data = read_input(INPUT_FILE)
+    assert part_1(input_data) == 0
+
+
+def test__part2_sample():
+    input_data = """
+    xxx
+    """
+    assert part_2(input_data) == 0
+
+
+def test__part2():
+    input_data = read_input(INPUT_FILE)
+    assert part_2(input_data) == 0
