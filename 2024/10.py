d9a0364450acb9e21038fa036071bf365a65076e
Joonas Pessi <joonas.pessi@mapbox.com>
2024-12-28 20:36:08 +0200

    day10 part2

--- 2024/10.py
+++ 2024/10.py
@@ -53,9 +53,34 @@ def part_1(input_data: str) -> int:
     return result
 
 
+def dynamic_programming(grid, y, x, cache):
+    if grid[y][x] == 0:
+        return 1
+    if (y, x) in cache:
+        return cache[(y, x)]
+    result = 0
+    for dy, dx in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        yy = y + dy
+        xx = x + dx
+        if (
+            0 <= yy < len(grid)
+            and 0 <= xx < len(grid[0])
+            and grid[yy][xx] == grid[y][x] - 1
+        ):
+            result = result + dynamic_programming(grid, yy, xx, cache)
+    cache[(y, x)] = result
+    return result
+
+
 def part_2(input_data: str) -> int:
-    data = parse(input_data)  # noqa
-    return 0
+    grid = parse(input_data)
+    result = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == 9:
+                result += dynamic_programming(grid, y, x, {})
+
+    return result
 
 
 def test__part1_sample():
@@ -77,18 +102,26 @@ def test__part1():
     assert part_1(input_data) == 574
 
 
-# def test__part2_sample():
-#     input_data = """
-#     xxx
-#     """
-#     assert part_2(input_data) == 0
+def test__part2_sample():
+    input_data = """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """
+    assert part_2(input_data) == 81
 
 
-# def test__part2():
-#     input_data = read_input(INPUT_FILE)
-#     assert part_2(input_data) == 0
+def test__part2():
+    input_data = read_input(INPUT_FILE)
+    assert part_2(input_data) == 1238
+
 
 if __name__ == "__main__":
     input_data = read_input(INPUT_FILE)
     print(part_1(input_data))
-    # print(part_2(input_data))
+    print(part_2(input_data))
