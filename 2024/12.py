afe5f3d9fcac480f78b298a1f02baf14827a188c
Joonas Pessi <joonas.pessi@mapbox.com>
2024-12-29 20:21:04 +0200

    day12 part2

--- 2024/12.py
+++ 2024/12.py
@@ -19,6 +19,8 @@ def parse(input_data):
 
 
 def part_1(input_data: str) -> int:
+    # up, right, down, left
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     grid = parse(input_data)
     r_len = len(grid)
     c_len = len(grid[0])
@@ -43,7 +45,7 @@ def part_1(input_data: str) -> int:
                 seen.add((rr, cc))
                 area += 1
 
-                for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                for dr, dc in directions:
                     rrr = rr + dr
                     ccc = cc + dc
                     if (
@@ -61,8 +63,69 @@ def part_1(input_data: str) -> int:
 
 
 def part_2(input_data: str) -> int:
-    data = parse(input_data)  # noqa
-    return 0
+    # up, right, down, left
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    grid = parse(input_data)
+    r_len = len(grid)
+    c_len = len(grid[0])
+
+    seen = set()
+
+    result = 0
+
+    for r in range(r_len):
+        for c in range(c_len):
+            if (r, c) in seen:
+                continue
+            q = deque([(r, c)])
+
+            area = 0
+            perim_c = {}
+
+            while q:
+                rr, cc = q.popleft()
+                if (rr, cc) in seen:
+                    continue
+                seen.add((rr, cc))
+                area += 1
+
+                for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                    rrr = rr + dr
+                    ccc = cc + dc
+                    if (
+                        0 <= rrr < r_len
+                        and 0 <= ccc < c_len
+                        and grid[rrr][ccc] == grid[rr][cc]
+                    ):
+                        q.append((rrr, ccc))
+                    else:
+                        if (dr, dc) not in perim_c:
+                            perim_c[(dr, dc)] = set()
+
+                        perim_c[(dr, dc)].add((rr, cc))
+
+            sides = 0
+            for cells in perim_c.values():
+                seen_perim = set()
+
+                for rp, cp in cells:
+                    if (rp, cp) in seen_perim:
+                        continue
+                    sides += 1
+                    q = deque([(rp, cp)])
+                    while q:
+                        rr, cc = q.popleft()
+                        if (rr, cc) in seen_perim:
+                            continue
+                        seen_perim.add((rr, cc))
+                        for dr, dc in directions:
+                            rrr, ccc = rr + dr, cc + dc
+                            if (rrr, ccc) in cells:
+                                q.append((rrr, ccc))
+
+            result += area * sides
+
+    return result
 
 
 def test__part1_sample():
@@ -86,19 +149,28 @@ def test__part1():
     assert part_1(input_data) == 1424472
 
 
-# def test__part2_sample():
-#     input_data = """
-#     xxx
-#     """
-#     assert part_2(input_data) == 0
+def test__part2_sample():
+    input_data = """
+    RRRRIICCFF
+    RRRRIICCCF
+    VVRRRCCFFF
+    VVRCCCJFFF
+    VVVVCJJCFE
+    VVIVCCJJEE
+    VVIIICJJEE
+    MIIIIIJJEE
+    MIIISIJEEE
+    MMMISSJEEE
+    """
+    assert part_2(input_data) == 1206
 
 
-# def test__part2():
-#     input_data = read_input(INPUT_FILE)
-#     assert part_2(input_data) == 0
+def test__part2():
+    input_data = read_input(INPUT_FILE)
+    assert part_2(input_data) == 870202
+
 
 if __name__ == "__main__":
-    test__part1()
-    # input_data = read_input(INPUT_FILE)
-    # print(part_1(input_data))
-    # print(part_2(input_data))
+    input_data = read_input(INPUT_FILE)
+    print(part_1(input_data))
+    print(part_2(input_data))
